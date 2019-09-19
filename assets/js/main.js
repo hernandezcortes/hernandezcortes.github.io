9fd9499e2d2d131f78f3a1a20aa0dfbacbf79e0f
Danae <44044091+hernandezcortes@users.noreply.github.com>
2019-09-19 18:20:06 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+
+
+
+$(document).ready(function() {
+
+    $('html').addClass('js-enabled');
+
+    $(window).load(function() {
+        $(".js-preloader").fadeOut(800, function() {
+            $(".js-main-container").fadeIn(800);
+
+            setup_scrollreveal();
+        });
+    });
+
+});
+
+
+function setup_scrollreveal()
+{
+    if(typeof ScrollReveal !== 'undefined' && $.isFunction(ScrollReveal)) {
+
+        window.sr = ScrollReveal();
+
+        var default_config = {
+            duration: 500,
+            delay: 0,
+            easing: 'ease',
+            scale: 1,
+            mobile: false
+        };
+        var header_config = $.extend(false, default_config, {
+            duration: 1200,
+            delay: 700
+        });
+
+        var default_delay = 175;
+
+        sr.reveal('.a-header', header_config, default_delay);
+
+    }
+
+}
